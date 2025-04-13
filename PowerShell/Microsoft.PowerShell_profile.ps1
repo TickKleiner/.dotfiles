fe4ce405d9d1d7ce769f53cf3d36c02d4af12feb
TKleiner <40608424+TickKleiner@users.noreply.github.com>
2025-04-13 18:11:10 +0300

    Scoop Search added

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,9 @@
-
-
-
 #f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
 Import-Module -Name Microsoft.WinGet.CommandNotFound
 #f45873b3-b655-43a6-b217-97c00aa0db58
+
 Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+
 oh-my-posh init pwsh --config ~/.dotfiles/PowerShell/Themes/catppuccin_mocha.omp.json | Invoke-Expression
+
+Invoke-Expression (&scoop-search --hook)
